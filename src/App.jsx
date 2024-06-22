28a1b845328e69c37b536a44494812da6a0796b2
kadek dika <dika.rama@undiksha.ac.id>
2024-06-22 12:29:34 +0800

    create Todos and TodoItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Todos from './components/Todos';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -27,10 +28,7 @@ function App() {
   return (
     <div>
       <h1>My Todo List</h1>
-            {/* Gunakan method map di sini */}
-            {todos.map((todo) => {
-        return <p key={todo.id}>{todo.title}</p>
-      })}
+      <Todos todos={todos} />
     </div>
   )
 }
